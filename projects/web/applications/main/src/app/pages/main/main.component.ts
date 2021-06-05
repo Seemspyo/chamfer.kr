85e5f70b41165d5594ad6572f05a2bdf68c65525
Wonpyo E <eunsatio@eunsatio.io>
2021-06-05 17:36:24 +0900

    conditional banner image

--- projects/web/applications/main/src/app/pages/main/main.component.ts
+++ projects/web/applications/main/src/app/pages/main/main.component.ts
@@ -1,13 +1,19 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { isPlatformBrowser } from '@angular/common';
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Inject,
+  OnDestroy,
+  OnInit,
   PLATFORM_ID
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Banner } from '@chamfer/server';
 import { SiteLayoutData } from 'common/api/configs/site-layout.config';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { MainData } from './main.resolver';
 
 
@@ -17,16 +23,21 @@ import { MainData } from './main.resolver';
   styleUrls: [ 'main.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy {
 
   public bannerList!: Banner[];
   public mediaConfig!: SiteLayoutData;
 
   public isBrowser: boolean;
+  public isMobile!: boolean;
+
+  private destroyed = new Subject<void>();
 
   constructor(
+    private changeDetector: ChangeDetectorRef,
     route: ActivatedRoute,
-    @Inject(PLATFORM_ID) platformId: Object
+    private mediaObserver: BreakpointObserver,
+    @Inject(PLATFORM_ID) platformId: Object,
   ) {
     const { bannerList, mediaConfig } = route.snapshot.data.mainData as MainData;
 
@@ -34,6 +45,28 @@ export class MainComponent {
     this.mediaConfig = mediaConfig;
 
     this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit() {
+    if (!this.isBrowser) return;
+
+    const query = '(max-width: 1028px)';
+
+    this.mediaObserver.observe(query)
+                      .pipe( takeUntil(this.destroyed) )
+                      .subscribe(state => {
+                        if (state.matches === this.isMobile) return;
+
+                        this.isMobile = state.matches;
+                        this.changeDetector.markForCheck();
+                      });
+
+    this.isMobile = this.mediaObserver.isMatched(query);
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
 }
